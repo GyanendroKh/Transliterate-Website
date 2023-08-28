3d3b1b4e7c7ee5b9a22c201d999607219abe25a7
Gyanendro Kh <me@gyanendrokh.dev>
2023-08-28 19:24:36 +0530

    Init Project

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  trailingComma: 'none'
+};
+
+module.exports = config;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-[100vh] items-center justify-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
