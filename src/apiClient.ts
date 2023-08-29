b0b14f6bbb131f0aa575ec4c72f724d37f6765ab
Gyanendro Kh <me@gyanendrokh.dev>
2023-08-29 15:25:35 +0530

    Changing API_URL

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -2,7 +2,7 @@ import { QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL || import.meta.env.BASE_URL
 });
 
 export const queryClient = new QueryClient();
